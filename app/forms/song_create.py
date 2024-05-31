d2d403a8880d79f691047158c3f37ec2c297fec1
Alex <gohalexx@gmail.com>
2024-05-30 18:37:51 -0700

    fixed validator syntax in SongForm

--- app/forms/song_create.py
+++ app/forms/song_create.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, NumberRange
 
 class SongForm(FlaskForm):
     song_name = StringField('song_name', validators=[DataRequired()])
-    duration = IntegerField('duration', validators=[DataRequired()], NumberRange(min=1))
+    duration = IntegerField('duration', validators=[DataRequired(), NumberRange(min=1)])
     song_url = StringField('song_url', validators=[DataRequired()])
     image_url = StringField('image_url', validators=[DataRequired()])
     artist_name = StringField('artist_name', validators=[DataRequired()])

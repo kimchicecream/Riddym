e21ee0ad88f7480b60887c4681dcd857e88e481d
Alex <gohalexx@gmail.com>
2024-05-30 18:34:08 -0700

    fixed no colon in TrackForm

--- app/forms/track_create.py
+++ app/forms/track_create.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
-class TrackForm(FlaskForm)
+class TrackForm(FlaskForm):
     song_id = IntegerField('song_id', validators=[DataRequired()])
     difficulty = StringField('difficulty', validators=[DataRequired()])
     duration = IntegerField('duration', validators=[DataRequired(), NumberRange(min=1)])

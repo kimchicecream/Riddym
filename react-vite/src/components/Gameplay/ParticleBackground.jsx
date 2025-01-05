d80aa9cdbbe7b5a5770461c955d0a2b50c27c181
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-04 19:20:51 -0700

    initiated ParticleBackground component

---
+++ react-vite/src/components/Gameplay/ParticleBackground.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import './ParticleBackground.css';
+
+function ParticleBackground() {
+    return (
+        <></>
+    )
+}
+
+export default ParticleBackground;

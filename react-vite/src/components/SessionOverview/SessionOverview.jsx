1f9ae796c7a7046742e18b5cc3aa7f4cc9112e1b
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-11 22:57:01 -0700

    commits

--- react-vite/src/components/SessionOverview/SessionOverview.jsx
+++ react-vite/src/components/SessionOverview/SessionOverview.jsx
@@ -20,7 +20,7 @@ function SessionOverview() {
     const userId = useSelector(state => state.session.user.id);
     const userScores = useSelector(state => state.scores.userScores);
 
-    // commit 2
+    // commit 3
 
     useEffect(() => {
         if (userId) {

420a03b55cb9eaecb8a9bbaca9f4019f6feda040
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-01-14 23:52:40 -0700

    recentered logo in navbar

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -24,7 +24,7 @@ function Navigation() {
   //   navigate(`/session-overview/${sessionUser.username}`);
   // };
 
-  // commit 4
+  // commit 5
 
   const handleSessionOverview = () => {
     navigate(`/session-overview/${sessionUser.username}`);
